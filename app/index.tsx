d986adbf1d1081a9ef3f08d84ccb031c2a796c73
yuvrajhere <yuvrajisbest13@gmail.com>
2024-12-27 17:37:55 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const index = () => {
+  return (
+    <View>
+      <Text className="text-red-500">Hello</Text>
+    </View>
+  );
+};
+
+export default index;
